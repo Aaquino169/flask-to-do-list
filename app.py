675548b72bc26498366cef59d79376d7902074ec
alexander_aquino <nanito169@gmail.com>
2020-12-22 12:08:56 -0500

    added db

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db= SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+
+    def __rept__(self):
+        return '<task %r >' % self.id
 
 @app.route('/')
 def index():
